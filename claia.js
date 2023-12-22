81310b7a5450893148cda74c3b8bf1de7a00db7a
Yug <ybpkrk@umsystem.edu>
2023-12-22 14:56:46 -0600

    added chalk and figlet"

--- claia.js
+++ claia.js
@@ -5,6 +5,8 @@ const { exec } = require('child_process');
 const readline = require('readline');
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
 const tempHistoryFilePath = path.join(__dirname, 'temp_history.json');
 
@@ -17,55 +19,56 @@ function initializeHistoryFile() {
 initializeHistoryFile();
 
 async function executeCommand(commandParts, rl, isInteractive) {
-    const commandPrompt = commandParts.join(' ');
-    console.log("Fetching command suggestion...");
-    const suggestedCommand = await fetchCommandFromOpenAI(commandPrompt);
+  const commandPrompt = commandParts.join(' ');
+  console.log("Fetching command suggestion...");
+  const suggestedCommand = await fetchCommandFromOpenAI(commandPrompt);
 
-    if (suggestedCommand.trim().toLowerCase() === 'error') {
-        console.log("No valid command was suggested.");
-        return isInteractive ? rl.prompt() : rl.close();
-    }
+  if (suggestedCommand.trim().toLowerCase() === 'error') {
+    console.log("No valid command was suggested.");
+    return isInteractive ? rl.prompt() : rl.close();
+  }
 
     console.log(`Suggested Command: ${suggestedCommand}`);
   
-    rl.question('Do you want to execute this command? (yes/no): ', (answer) => {
-        if (answer.toLowerCase() === 'yes') {
-            console.log('Executing command...');
-            exec(suggestedCommand, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Error: ${error.message}`);
-                }
-                if (stderr) {
-                    console.error(`Stderr: ${stderr}`);
-                }
-                console.log(stdout ? `Stdout: ${stdout}` : "Stdout: None");
-                if (!isInteractive) {
-                    rl.close();  // Close the readline interface if not in interactive mode
-                } else {
-                    rl.prompt(); // Return to the prompt in interactive mode
-                }
-            });
-        } else {
-            console.log('Command execution cancelled.');
-            if (!isInteractive) {
-                rl.close();  // Close the readline interface if not in interactive mode
-            } else {
-                rl.prompt(); // Return to the prompt in interactive mode
-            }
+  rl.question('Do you want to execute this command? (yes/no): ', (answer) => {
+    if (answer.toLowerCase() === 'yes') {
+      console.log('Executing command...');
+      exec(suggestedCommand, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
         }
-    });
+        if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+        }
+        console.log(stdout ? `Stdout: ${stdout}` : "Stdout: None");
+        if (!isInteractive) {
+          rl.close(); 
+        } else {
+          rl.prompt();
+        }
+      });
+    } 
+    else {
+      console.log('Command execution cancelled.');
+      if (!isInteractive) {
+        rl.close();  
+      } else {
+        rl.prompt(); 
+      }
+    }
+  });
 }
 
 function viewHistory() {
-    if (fs.existsSync(tempHistoryFilePath)) {
-        const history = JSON.parse(fs.readFileSync(tempHistoryFilePath, 'utf8'));
-        history.forEach((item, index) => {
-            console.log(`Q${index + 1}: ${item.question}`);
-            console.log(`A${index + 1}: ${item.answer}\n`);
-        });
-    } else {
-        console.log("No history found.");
-    }
+  if (fs.existsSync(tempHistoryFilePath)) {
+    const history = JSON.parse(fs.readFileSync(tempHistoryFilePath, 'utf8'));
+    history.forEach((item, index) => {
+      console.log(`Q${index + 1}: ${item.question}`);
+      console.log(`A${index + 1}: ${item.answer}\n`);
+    });
+  } else {
+    console.log("No history found.");
+  }
 }
 
 function clearHistory() {
@@ -74,38 +77,41 @@ function clearHistory() {
 }
 
 function startInteractiveMode() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: 'claia> '
-    });
+  console.clear();
+  console.log(chalk.hex('#008080').bold(figlet.textSync('CLAIA', { horizontalLayout: 'full' })));
 
-    console.log("Interactive mode. Type 'exit' to leave.");
-    rl.prompt();
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: chalk.hex('#008080')('claia> ')
+  });
 
-    rl.on('line', async (line) => {
-        if (line.trim() === 'exit') {
-            rl.close();
-        } else if (line.startsWith('execute') || line.startsWith('ex')) {
-            await executeCommand(line.split(' ').slice(1), rl, true);
-        } else if (line.trim() === 'clear' || line.trim() === 'clr') {
-            clearHistory();
-            rl.prompt();
-        } else if (line.trim() === 'history' || line.trim() === 'h') {
-            viewHistory();
-            rl.prompt();
-        } else if (line.trim() === 'help') {
-            console.log(program.helpInformation());
-            rl.prompt();
-        } else {
-            const response = await fetchResponseFromOpenAI(line.trim());
-            console.log(response);
-            rl.prompt();
-        }
-    }).on('close', () => {
-        console.log('Exiting interactive mode.');
-        process.exit(0);
-    });
+  console.log("Interactive mode. Type 'exit' to leave.");
+  rl.prompt();
+
+  rl.on('line', async (line) => {
+    if (line.trim() === 'exit') {
+        rl.close();
+    } else if (line.startsWith('execute') || line.startsWith('ex')) {
+        await executeCommand(line.split(' ').slice(1), rl, true);
+    } else if (line.trim() === 'clear' || line.trim() === 'clr') {
+        clearHistory();
+        rl.prompt();
+    } else if (line.trim() === 'history' || line.trim() === 'h') {
+        viewHistory();
+        rl.prompt();
+    } else if (line.trim() === 'help') {
+        console.log(program.helpInformation());
+        rl.prompt();
+    } else {
+        const response = await fetchResponseFromOpenAI(line.trim());
+        console.log(response);
+        rl.prompt();
+    }
+  }).on('close', () => {
+      console.log('Exiting interactive mode.');
+      process.exit(0);
+  });
 }
 
 program
